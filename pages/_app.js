2ac18eb3061bc24b67c1130e956d747b9eed14bb
arealclimber <luffulgence@gmail.com>
2022-07-10 16:21:48 +0800

    Wrap the entire app inside the AppContext.Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
-import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
+import { useState } from 'react';
+import AppContext from '../AppContext';
+import languagesObject from '../languageObject';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	const [languageSelected, setLanguageSelected] = useState('en');
+	const languageObject = languagesObject;
+	return (
+		<AppContext.Provider
+			value={{
+				state: {
+					languages: languageObject[languageSelected],
+					languageSelected: languageSelected,
+				},
+				setLanguageSelected: setLanguageSelected,
+			}}
+		>
+			<Component {...pageProps} />
+		</AppContext.Provider>
+	);
 }
 
 export default MyApp;
